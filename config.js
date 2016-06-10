3d6131454418d0d55269864b0493483b83294cfb
Vincent Ning <vincent.ning93@gmail.com>
2016-06-10 14:31:30 -0700

    fresh app

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
+/**
+ * Modify the config Scaphold URL to point to your specific app.
+ * Find the URL at the top of the page on Scaphold.io once you've created an app.
+ * Yup. It's that easy.
+ */
+
 var config = {
-	scapholdUrl: "https://api.scaphold.io/graphql/9ef14f35-c459-41e9-a9d3-3933a7149822",
+	scapholdUrl: "https://api.scaphold.io/graphql/d8684aa2-cc15-42ec-99bb-a151faac61d5",
 }
 
 module.exports = config;
